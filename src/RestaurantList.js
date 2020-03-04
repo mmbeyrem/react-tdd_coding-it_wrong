7ffa01f98e0f6d84fa1a3fe738cc6cf50e9a4dc9
Beyrem <mmakhlouf@ecovadis.com>
2020-03-04 19:14:04 +0100

    style resatuant list

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Collection, CollectionItem } from "react-materialize";
 const RestaurantList = ({ restaurants }) => (
     <>
-        <h3>Restaurants</h3>
-        <Collection>
+        <Collection header="Restaurants">
+            {
+                restaurants.length === 0
+                && <CollectionItem style={{ color: 'gray' }} > (no restaurants saved. )   </CollectionItem>
+            }
             {
                 restaurants.map(r => (
                     <CollectionItem key={r}>
