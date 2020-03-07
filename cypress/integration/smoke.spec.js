f0ad9641b414ac82b38798f232edaee1a01f4fd9
Beyrem <mmakhlouf@ecovadis.com>
2020-03-07 15:14:18 +0100

    fix build

--- cypress/integration/smoke.spec.js
+++
@@ -1,9 +0,0 @@
-describe("smoke tests", () => {
-
-    it("Makes sure the welcome message comes up !", () => {
-        cy.visit('http://localhost:1234')
-            .contains("Hello word");
-
-    });
-
-});
