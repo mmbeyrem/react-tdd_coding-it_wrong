3c2da41e13c9b02dd9e911a3bea955f5b9facd31
Beyrem <mmakhlouf@ecovadis.com>
2020-03-07 14:39:22 +0100

    add run p

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -40877,7 +40877,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54171" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59176" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
